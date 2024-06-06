d6af07496e089defa430ed4f0b1ec264825f584d
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2024-06-06 10:39:15 -0300

    nivelHambre complete y valorAlimentario complete

---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public int nivelHambre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void tomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+        if (nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            tomarAlimento(alimento.valorAlimentario);
+        }
+    }
+}
